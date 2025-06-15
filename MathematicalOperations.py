d3c962f9d3bec0d71996425a683202b4daf01c7d
Sanjay-Kumar-G <skgone01@gmail.com>
2025-06-15 23:38:24 +0530

    Math Operarions

---
+++ MathematicalOperations.py
@@ -0,0 +1,12 @@
+
+Num1 =int(input("Enter the first number: "))
+Num2 =int(input("Enter the second number: "))
+
+Sum = Num1 + Num2
+print("Additionm", Sum)
+Sub = Num1 - Num2
+print("Subtraction", Sub)
+Mul = Num1 * Num2
+print("Multiplication", Mul)
+Div = Num1 / Num2
+print("Division", Div)
