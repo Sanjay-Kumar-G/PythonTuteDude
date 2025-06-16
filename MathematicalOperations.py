6ae95593222e1c6db67e3eb5b448b84b4064d33b
Sanjay-Kumar-G <skgone01@gmail.com>
2025-06-16 15:28:51 +0530

    Update MathematicalOperations.py

--- MathematicalOperations.py
+++ MathematicalOperations.py
@@ -3,10 +3,10 @@ Num1 =int(input("Enter the first number: "))
 Num2 =int(input("Enter the second number: "))
 
 Sum = Num1 + Num2
-print("Additionm", Sum)
+print("Addition: ", Sum)
 Sub = Num1 - Num2
-print("Subtraction", Sub)
+print("Subtraction: ", Sub)
 Mul = Num1 * Num2
-print("Multiplication", Mul)
+print("Multiplication:", Mul)
 Div = Num1 / Num2
-print("Division", Div)
+print("Division: ", Div)
